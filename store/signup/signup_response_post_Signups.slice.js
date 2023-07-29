18d268345ca26ecd0c7747c11d3ed9d0d44ddf2e
Crowdbotics <engineering@crowdbotics.com>
2023-07-29 08:39:23 +0000

    Update store.

--- store/signup/signup_response_post_Signups.slice.js
+++ store/signup/signup_response_post_Signups.slice.js
@@ -1,32 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const rest_auth_password_reset_confirm_create = createAsyncThunk(
-  "passwordResetConfirms/rest_auth_password_reset_confirm_create",
+export const signup_post_api_v1_signup_read = createAsyncThunk(
+  "signup_response_post_Signups/signup_post_api_v1_signup_read",
   async payload => {
-    const response = await apiService.rest_auth_password_reset_confirm_create(
-      payload
-    )
+    const response = await apiService.signup_post_api_v1_signup_read(payload)
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const passwordResetConfirmsSlice = createSlice({
-  name: "passwordResetConfirms",
+const signup_response_post_SignupsSlice = createSlice({
+  name: "signup_response_post_Signups",
   initialState,
   reducers: {},
   extraReducers: {
-    [rest_auth_password_reset_confirm_create.pending]: (state, action) => {
+    [signup_post_api_v1_signup_read.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [rest_auth_password_reset_confirm_create.fulfilled]: (state, action) => {
+    [signup_post_api_v1_signup_read.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities.push(action.payload)
         state.api.loading = "idle"
       }
     },
-    [rest_auth_password_reset_confirm_create.rejected]: (state, action) => {
+    [signup_post_api_v1_signup_read.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -35,6 +33,6 @@ const passwordResetConfirmsSlice = createSlice({
   }
 })
 export default {
-  rest_auth_password_reset_confirm_create,
-  slice: passwordResetConfirmsSlice
+  signup_post_api_v1_signup_read,
+  slice: signup_response_post_SignupsSlice
 }
